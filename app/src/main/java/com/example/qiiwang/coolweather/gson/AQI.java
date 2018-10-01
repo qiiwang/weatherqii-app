a970c1dc4d68a5eb643315d195ce23ae5ff58f4c
qii.wang <jinjian821@gmail.com>
2018-10-02 06:57:26 +0800

    Second commit.

---
+++ app/src/main/java/com/example/qiiwang/coolweather/gson/AQI.java
@@ -0,0 +1,8 @@
+package com.example.qiiwang.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/10/1 0001.
+ */
+
+public class AQI {
+}
---
+++ app/src/main/java/com/example/qiiwang/coolweather/util/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.example.qiiwang.coolweather.util;
+
+/**
+ * Created by Administrator on 2018/10/1 0001.
+ */
+
+public class HttpUtil {
+}
